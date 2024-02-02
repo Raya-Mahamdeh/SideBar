07a41de9fdbf4eeae01d96258969addc6b72a019
Raya Mahamdeh <55127570+Raya-Mahamdeh@users.noreply.github.com>
2024-02-02 18:53:20 +0200

    DONE

---
+++ javascript.js
@@ -0,0 +1,11 @@
+    //  open navigation
+function openNavbar() {
+    document.getElementById("SideNavbar").style.width = "400px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+  }
+    //  close navigation
+function closeNavbar() {
+    document.getElementById("SideNavbar").style.width = "0";
+    document.body.style.backgroundColor = "white";
+
+  }
